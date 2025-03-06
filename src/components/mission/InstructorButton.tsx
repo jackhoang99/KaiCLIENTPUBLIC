bcb41d03c306c20b577c7bf3212c7f2507a73326
jackhoang99 <114491294+jackhoang99@users.noreply.github.com>
2025-03-05 23:53:20 -0800

    updates

--- src/components/mission/InstructorButton.tsx
+++ src/components/mission/InstructorButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { ArrowRight } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import ButtonContainer from './ButtonContainer';
+import ButtonContainer from "./ButtonContainer";
 
 const InstructorButton = () => {
   const navigate = useNavigate();
@@ -20,7 +20,7 @@ const InstructorButton = () => {
   return (
     <ButtonContainer>
       <motion.button
-        onClick={() => handleNavigation('/schedule')}
+        onClick={() => handleNavigation("/schedule")}
         className="group flex items-center justify-between w-full border border-[#33291C] rounded-full px-8 py-6 hover:bg-[#33291C] hover:text-white transition-all duration-300 active:scale-[0.98] mb-0"
         whileHover="hover"
         initial="initial"
